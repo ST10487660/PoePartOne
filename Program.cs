b54a0993d5998fabb9a140f4e3d42ed9d93c0cd1
Matlou Shaun Rakgoale <St10487660@rcconect.edu.za>
2026-04-13 14:37:17 +0200

    named the project title

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {// This is the main entry point of the application. It  sets up the console title, runs the introduction to get the user's name, and starts the chatbot conversation.
         static void Main(string[] args)
         {
-            Console.Title = "Cybersecurity Awareness Bot";
+            Console.Title = "Cybersecurity Awareness ChatBot";
 
             VoiceLogo intro = new VoiceLogo(); //outside of main method, creates instance of VoiceLogo class to run the introduction sequence and get the user's name.
             string userName = intro.RunIntro(); // gets name from intro sequence
